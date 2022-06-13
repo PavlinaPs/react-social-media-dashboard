1578cfb74f663f05b3cbcbbfbd11bb3d3676a57d
PPS <pavlina.psarsky@gmail.com>
2022-06-13 22:40:36 +0200

    heading done

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -3,9 +3,11 @@ import React from 'react'
 const Heading = ({ lightModeOn, setLightModeOn, handleChangeTheme }) => {
 
   return (
-    <header className="Dashboard__Heading">
-        <h1 className="Heading__title">Social Media Dashboard</h1>
-        <p className="Heading__text">Total Followers: 23,004</p>
+    <header className="Heading">
+        <div className="heading__title-container">
+            <h1 className="Heading__title">Social Media Dashboard</h1>
+            <p className="Heading__text">Total Followers: 23,004</p>
+        </div>
         <div className="Heading__toggle--container">
             <div 
                 onClick={handleChangeTheme}
